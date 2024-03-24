887d78ec17fc653f6972151f4a1ef32934f0f585
Nico <nicolas2289h@gmail.com>
2024-03-23 23:07:40 -0300

    se agrego el proyecto Gestion de Empleados

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -11,6 +11,8 @@ import ImgPaginaWeb from '../assets/img-work/img-pagina-web.png'
 import ImgReservas from '../assets/img-work/img-meriendas.png'
 import ImgDashboard from '../assets/img-work/img-dashboard.png'
 import ImgJuegoJs from '../assets/img-work/img-juego-memoria.png'
+import ImgGestor from '../assets/img-work/img-gestion-empleados.png'
+import ImgCalculadora from '../assets/img-work/img-calculadora.png'
 
 const Work = () => {
   return (
@@ -34,35 +36,35 @@ const Work = () => {
 
             <div className='flex gap-4 lg:mt-0 mt-8 justify-end flex-wrap items-end relative'>
               <a href='https://pagina-reservas-5be219dig-nicolas2289h.vercel.app/' rel='noreferrer noopener' target='blank' className='group relative overflow-hidden border-2 border-white/50 rounded-md lg:w-[31.5%] lg:h-[180px] cursor-pointer' >
-                <div className='group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300'></div>
+                <div className='group-hover:bg-black/85 w-full h-full absolute z-40 transition-all duration-300'></div>
                 <img className='group-hover:scale-110 transition-all duration-500 lg:h-[180px] object-cover' src={ImgReservas} alt="" />
-                <div className='absolute -bottom-full left-7 group-hover:bottom-14 transition-all duration-500 z-50'>
-                  <span className='text-gradient text-sm'>Sistema para reserva de meriendas de un local comercial.</span>
+                <div className='absolute -bottom-full left-4 group-hover:bottom-20 transition-all duration-500 z-50'>
+                  <span className='text-md font-semibold'>Sistema para reserva de meriendas de un local comercial.</span>
                 </div>
-                <div className='absolute -bottom-full left-7 group-hover:bottom-5 transition-all duration-700 z-50'>
-                  <span className='text-gradient text-sm'>#React #Boostrap #Firebase</span>
+                <div className='absolute -bottom-full left-4 group-hover:bottom-9 transition-all duration-700 z-50'>
+                  <span className='text-md font-semibold'>#React #Boostrap #Firebase</span>
                 </div>
               </a>
 
               <a href='https://todolist-react-mu.vercel.app/' rel='noreferrer noopener' target='blank' className='group relative overflow-hidden border-2 border-white/50 rounded-md lg:w-[31.5%] lg:h-[180px] cursor-pointer' >
-                <div className='group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300'></div>
+                <div className='group-hover:bg-black/80 w-full h-full absolute z-40 transition-all duration-300'></div>
                 <img className='group-hover:scale-110 transition-all duration-500 lg:h-[180px] object-cover' src={ImgTodolist} alt="" />
-                <div className='absolute -bottom-full left-7 group-hover:bottom-20 transition-all duration-500 z-50'>
-                  <span className='text-gradient text-sm'>Aplicación de Listado de tareas con modal y localStorage.</span>
+                <div className='absolute -bottom-full left-4 group-hover:bottom-20 transition-all duration-500 z-50'>
+                  <span className='text-md font-semibold'>Aplicación para Listado de tareas con localStorage.</span>
                 </div>
-                <div className='absolute -bottom-full left-7 group-hover:bottom-10 transition-all duration-700 z-50'>
-                  <span className='text-gradient text-sm'>#React #Boostrap</span>
+                <div className='absolute -bottom-full left-4 group-hover:bottom-10 transition-all duration-700 z-50'>
+                  <span className='text-md font-semibold'>#React #Boostrap</span>
                 </div>
               </a>
 
               <a href='https://pagina-web-servicios.vercel.app/' rel='noreferrer noopener' target='blank' className='group relative overflow-hidden border-2 border-white/50 rounded-md lg:w-[31.5%] lg:h-[180px] cursor-pointer' >
-                <div className='group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300'></div>
-                <img className='group-hover:scale-110 transition-all duration-500 lg:h-[180px] object-cover' src={ImgPaginaWeb} alt="" />
-                <div className='absolute -bottom-full left-7 group-hover:bottom-14 transition-all duration-500 z-50'>
-                  <span className='text-gradient text-sm'>Diseño de página web responsive con menu desplegable.</span>
+                <div className='group-hover:bg-black/80 w-full h-full absolute z-40 transition-all duration-300'></div>
+                <img className='group-hover:scale-110 transition-all duration-500 lg:h-[180px] object-cover' src={ImgGestor} alt="" />
+                <div className='absolute -bottom-full left-4 group-hover:bottom-20 transition-all duration-500 z-50'>
+                  <span className='text-md font-semibold'>Sistema de Gestion de Empleados con operaciones CRUD</span>
                 </div>
-                <div className='absolute -bottom-full left-7 group-hover:bottom-5 transition-all duration-700 z-50'>
-                  <span className='text-gradient text-sm'>#HTML #CSS #Javascript</span>
+                <div className='absolute -bottom-full left-4 group-hover:bottom-10 transition-all duration-700 z-50'>
+                  <span className='text-md font-semibold'>#React #SpringBoot #Boostrap</span>
                 </div>
               </a>
             </div>
@@ -77,46 +79,46 @@ const Work = () => {
 
             <div className='flex gap-4 lg:mb-0 lg:flex-row flex-col lg:mt-0 mt-4'>
               <a href='https://carrito-compras-pi.vercel.app/' rel="noopener noreferrer" target='blank' className='group relative overflow-hidden border-2 border-white/50 rounded-md lg:w-[40%] lg:h-[180px] '>
-                <div className='group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300'></div>
+                <div className='group-hover:bg-black/80 w-full h-full absolute z-40 transition-all duration-300'></div>
                 <img className='group-hover:scale-110 transition-all duration-500 lg:h-[180px] object-cover' src={ImgTiendaVirtual} alt="" />
-                <div className='absolute -bottom-full left-7 group-hover:bottom-20 transition-all duration-500 z-50'>
-                  <span className='text-gradient text-sm'>Diseño de una Tienda virtual básica con carrito de compras.</span>
+                <div className='absolute -bottom-full left-4 group-hover:bottom-20 transition-all duration-500 z-50'>
+                  <span className='text-md font-semibold'>Diseño de una Tienda virtual básica con carrito de compras.</span>
                 </div>
-                <div className='absolute -bottom-full left-7 group-hover:bottom-10 transition-all duration-700 z-50'>
-                  <span className='text-gradient text-sm'>#React #CSS</span>
+                <div className='absolute -bottom-full left-4 group-hover:bottom-10 transition-all duration-700 z-50'>
+                  <span className='text-md font-semibold'>#React #CSS</span>
                 </div>
               </a>
 
               <a href='https://encriptador-cifrado-cesar.vercel.app/' rel="noopener noreferrer" target='blank' className='group relative overflow-hidden border-2 border-white/50 rounded-md lg:w-[40%] lg:h-[180px] '>
-                <div className='group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300'></div>
+                <div className='group-hover:bg-black/80 w-full h-full absolute z-40 transition-all duration-300'></div>
                 <img className='group-hover:scale-110 transition-all duration-500 lg:h-[180px] object-cover' src={ImgCifrado} alt="" />
-                <div className='absolute -bottom-full left-7 group-hover:bottom-14 transition-all duration-500 z-50'>
-                  <span className='text-gradient text-sm'>Encriptador realizado como parte de un challenge de la empresa Alura Latam.</span>
+                <div className='absolute -bottom-full left-4 group-hover:bottom-14 transition-all duration-500 z-50'>
+                  <span className='text-md font-semibold'>Encriptador realizado como parte de un challenge de la empresa Alura Latam.</span>
                 </div>
-                <div className='absolute -bottom-full left-7 group-hover:bottom-5 transition-all duration-700 z-50'>
-                  <span className='text-gradient text-sm'>#HTML #CSS #Javascript</span>
+                <div className='absolute -bottom-full left-4 group-hover:bottom-5 transition-all duration-700 z-50'>
+                  <span className='text-md font-semibold'>#HTML #CSS #Javascript</span>
                 </div>
               </a>
 
               <a href='https://weather-app-seven-pearl-45.vercel.app/' rel="noopener noreferrer" target='blank' className='group relative overflow-hidden border-2 border-white/50 rounded-md lg:w-[40%] lg:h-[180px] '>
-                <div className='group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300'></div>
+                <div className='group-hover:bg-black/80 w-full h-full absolute z-40 transition-all duration-300'></div>
                 <img className='group-hover:scale-110 transition-all duration-500 lg:h-[180px] object-cover' src={ImgClima} alt="" />
-                <div className='absolute -bottom-full left-7 group-hover:bottom-14 transition-all duration-500 z-50'>
-                  <span className='text-gradient text-sm'>Aplicacion que muestra el <br /> clima en tiempo real mediante una API.</span>
+                <div className='absolute -bottom-full left-4 group-hover:bottom-14 transition-all duration-500 z-50'>
+                  <span className='text-md font-semibold'>Aplicacion que muestra el <br /> clima en tiempo real mediante una API.</span>
                 </div>
-                <div className='absolute -bottom-full left-7 group-hover:bottom-5 transition-all duration-700 z-50'>
-                  <span className='text-gradient text-sm'>#HTML #CSS #Javascript</span>
+                <div className='absolute -bottom-full left-4 group-hover:bottom-5 transition-all duration-700 z-50'>
+                  <span className='text-md font-semibold'>#HTML #CSS #Javascript</span>
                 </div>
               </a>
 
               <a href='https://dashboard-weld-iota.vercel.app/' rel="noopener noreferrer" target='blank' className='group relative overflow-hidden border-2 border-white/50 rounded-md lg:w-[40%] lg:h-[180px] '>
-                <div className='group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300'></div>
+                <div className='group-hover:bg-black/80 w-full h-full absolute z-40 transition-all duration-300'></div>
                 <img className='group-hover:scale-110 transition-all duration-500 lg:h-[180px] object-cover' src={ImgDashboard} alt="" />
-                <div className='absolute -bottom-full left-7 group-hover:bottom-20 transition-all duration-500 z-50'>
-                  <span className='text-gradient text-sm'>Dashboard responsivo con gráfico dinámico.</span>
+                <div className='absolute -bottom-full left-4 group-hover:bottom-20 transition-all duration-500 z-50'>
+                  <span className='text-md font-semibold'>Dashboard responsivo con gráficos dinámicos.</span>
                 </div>
-                <div className='absolute -bottom-full left-7 group-hover:bottom-5 transition-all duration-700 z-50'>
-                  <span className='text-gradient text-sm'>#HTML #CSS #Javascript <br />#DarkTheme</span>
+                <div className='absolute -bottom-full left-4 group-hover:bottom-5 transition-all duration-700 z-50'>
+                  <span className='text-md font-semibold'>#HTML #CSS #Javascript <br />#DarkTheme</span>
                 </div>
               </a>
             </div>
@@ -131,25 +133,47 @@ const Work = () => {
             viewport={{ once: false, amount: 0.3 }}>
 
             <div className='flex gap-4 lg:mb-0 lg:flex-row flex-col lg:mt-0 mt-4'>
-              <a href='https://pagina-reservas.vercel.app/' rel="noopener noreferrer" target='blank' className='group relative overflow-hidden border-2 border-white/50 rounded-md lg:w-[34.4%] lg:h-[180px] '>
-                <div className='group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300'></div>
-                <img className='group-hover:scale-110 transition-all duration-500 lg:h-[180px] object-cover' src={ImgJuegoJs} alt="" />
-                <div className='absolute -bottom-full left-7 group-hover:bottom-20 transition-all duration-500 z-50'>
-                  <span className='text-gradient text-sm'>Juego de memoria implementado con Javascript.</span>
+              <a href='https://nh-employees.vercel.app/' rel="noopener noreferrer" target='blank' className='group relative overflow-hidden border-2 border-white/50 rounded-md lg:w-[34.4%] lg:h-[180px] '>
+                <div className='group-hover:bg-black/80 w-full h-full absolute z-40 transition-all duration-300'></div>
+                <img className='group-hover:scale-110 transition-all duration-500 lg:h-[180px] object-cover' src={ImgPaginaWeb} alt="" />
+                <div className='absolute -bottom-full left-4 group-hover:bottom-20 transition-all duration-500 z-50'>
+                  <span className='text-md font-semibold'>Diseño de página web responsive con menu desplegable.</span>
                 </div>
-                <div className='absolute -bottom-full left-7 group-hover:bottom-10 transition-all duration-700 z-50'>
-                  <span className='text-gradient text-sm'>#HTML #CSS #Javascript</span>
+                <div className='absolute -bottom-full left-4 group-hover:bottom-9 transition-all duration-700 z-50'>
+                  <span className='text-md font-semibold'>#HTML #CSS #Javascript</span>
+                </div>
+              </a>
+
+              <a href='https://juego-de-memoria-blond.vercel.app/' rel="noopener noreferrer" target='blank' className='group relative overflow-hidden border-2 border-white/50 rounded-md lg:w-[34.4%] lg:h-[180px] '>
+                <div className='group-hover:bg-black/80 w-full h-full absolute z-40 transition-all duration-300'></div>
+                <img className='group-hover:scale-110 transition-all duration-500 lg:h-[180px] object-cover' src={ImgJuegoJs} alt="" />
+                <div className='absolute -bottom-full left-4 group-hover:bottom-20 transition-all duration-500 z-50'>
+                  <span className='text-md font-semibold'>Juego de memoria implementado con Javascript.</span>
+                </div>
+                <div className='absolute -bottom-full left-4 group-hover:bottom-10 transition-all duration-700 z-50'>
+                  <span className='text-md font-semibold'>#HTML #CSS #Javascript</span>
+                </div>
+              </a>
+
+              <a href='https://nh-calculadora.vercel.app/' rel="noopener noreferrer" target='blank' className='group relative overflow-hidden border-2 border-white/50 rounded-md lg:w-[34.4%] lg:h-[180px] '>
+                <div className='group-hover:bg-black/80 w-full h-full absolute z-40 transition-all duration-300'></div>
+                <img className='group-hover:scale-110 transition-all duration-500 lg:h-[180px] object-cover' src={ImgCalculadora} alt="" />
+                <div className='absolute -bottom-full left-4 group-hover:bottom-20 transition-all duration-500 z-50'>
+                  <span className='text-md font-semibold'>Calculadora implementada con React.</span>
+                </div>
+                <div className='absolute -bottom-full left-4 group-hover:bottom-10 transition-all duration-700 z-50'>
+                  <span className='text-md font-semibold'>#React #FramerMotion</span>
                 </div>
               </a>
 
               <a href='https://pagina-web-responsive.vercel.app/' rel="noopener noreferrer" target='blank' className='group relative overflow-hidden border-2 border-white/50 rounded-md lg:w-[34.4%] lg:h-[180px] '>
-                <div className='group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300'></div>
+                <div className='group-hover:bg-black/80 w-full h-full absolute z-40 transition-all duration-300'></div>
                 <img className='group-hover:scale-110 transition-all duration-500 lg:h-[180px] object-cover' src={ImgPagServicios} alt="" />
-                <div className='absolute -bottom-full left-7 group-hover:bottom-20 transition-all duration-500 z-50'>
-                  <span className='text-gradient text-sm'>Diseño de página web con video loop de portada.</span>
+                <div className='absolute -bottom-full left-4 group-hover:bottom-20 transition-all duration-500 z-50'>
+                  <span className='text-md font-semibold'>Diseño de página web con video loop de portada.</span>
                 </div>
-                <div className='absolute -bottom-full left-7 group-hover:bottom-10 transition-all duration-700 z-50'>
-                  <span className='text-gradient text-sm'>#HTML #CSS #Javascript</span>
+                <div className='absolute -bottom-full left-4 group-hover:bottom-10 transition-all duration-700 z-50'>
+                  <span className='text-md font-semibold'>#HTML #CSS #Javascript</span>
                 </div>
               </a>
             </div>
